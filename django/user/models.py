4515ef8369b07d7cdceee89f7f9c3d7fd1003211
andreydmitr20 <116160458+andreydmitr20@users.noreply.github.com>
2023-06-03 23:17:07 +0200

    Update models.py

--- django/user/models.py
+++ django/user/models.py
@@ -5,6 +5,11 @@ from django.db import models
 class User (models.Model):
     """ user """
 
+    user_id = models.BigAutoField(
+        primary_key=True,
+        db_column='user_id',
+    )
+
     username = models.CharField(
         max_length=40,
         unique=True,
@@ -24,6 +29,14 @@ class User (models.Model):
         null=False,
         blank=True,
         auto_now_add=True
+    )
+
+    list_display = (
+        'user_id',
+        'username',
+        # 'password',
+        'public_info',
+        'date_creation',
     )
 
     def __str__(self):
