b85457ab5fa63eaee06156f0139d94a4f1e64927
andreydmitr21 <andreydmitr20@gmail.com>
2023-07-12 20:13:01 +0200

    uwsgi docker compose done

--- entrypoint.server.sh
+++ entrypoint.server.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 LANG=en_US.UTF-8,LC_ALL=en_US.UTF-8
 PYTHONPATH=/lib/
-#alembic upgrade head
-gunicorn -c ini.gunicorn.py --bind 0.0.0.0:8080 app:gunicorn_app
+
+cd django
+python3 m migrate
+cd ..
+
+uwsgi --ini uwsgi.ini
 exec "$@"
